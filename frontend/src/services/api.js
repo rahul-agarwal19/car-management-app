3c1c63f48486ea6919e256816a98cf1d0f917612
Rahul <rahulkagarwal2002@gmail.com>
2024-11-19 22:07:25 +0530

    Fixed API endpoint configuration

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://car-management-backend-qr8i.onrender.com',
+  baseURL: 'https://car-management-backend-qr8i.onrender.com/api',
   withCredentials: false
 });
 
